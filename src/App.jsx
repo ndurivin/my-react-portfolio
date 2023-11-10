1ef5eba7480331bc72d03f656713a6d70393969d
ndurivin <ndurivo@gmail.com>
2023-11-10 08:15:39 +0300

    Adds the banner section and hero sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Banner from "./components/Banner";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Banner />
+      <Hero />
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
+      <nav className="container-fluid navbar navbar-expand-lg bg-body-tertiary fixed-top">
+        <div className="container">
           <a className="navbar-brand" href="#">
             VINN
           </a>
@@ -17,7 +17,7 @@ const Navbar = () => {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="collapse navbar-collapse justify-end" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">
